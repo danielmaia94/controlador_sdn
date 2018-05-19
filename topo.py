fda0d26dbe46fb783effb72b0e006d1387f96f1c
None <None>
2018-05-19 09:00:25 -0700

    alteracao topologia

--- topo.py
+++ topo.py
@@ -9,11 +9,9 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts
-	o_cdn1 = self.addHost('h1')
-	o_cdn2 = self.addHost('h2')
+	user1 = self.addHost('h1')
+	user2 = self.addHost('h2')
 	cdn = self.addHost('h3')
-	user_1 = self.addHost('h4')
-	user_2 = self.addHost('h5')
 
 	# Add switches
         s1 = self.addSwitch('s1')
@@ -22,30 +20,16 @@ class MyTopo( Topo ):
         s4 = self.addSwitch('s4')
         s5 = self.addSwitch('s5')
         s6 = self.addSwitch('s6')
-        s7 = self.addSwitch('s7')
-        s8 = self.addSwitch('s8')
-        s9 = self.addSwitch('s9')
-        s10 = self.addSwitch('s10')
-	s11 = self.addSwitch('s11')
 
         # Add links
         self.addLink(s1, s2)
         self.addLink(s1, s3)
-        self.addLink(s2, s3)
+        self.addLink(s3, s2)
+        self.addLink(s2, cdn)
         self.addLink(s3, s4)
         self.addLink(s4, s5)
         self.addLink(s4, s6)
-        self.addLink(s5, s6)
-        self.addLink(s6, o_cdn1)
-        self.addLink(s6, s8)
-        self.addLink(s8, o_cdn2)
-	self.addLink(s5, s7)
-	self.addLink(s7, s9)
-	self.addLink(s8, s10)
-	self.addLink(s10, s11)
-	self.addLink(s9, s11)
-	self.addLink(s11, cdn)
-	self.addLink(s11, user_1)
-	self.addLink(s11, user_2)
+        self.addLink(s6, user1)
+        self.addLink(s5, user2)
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
