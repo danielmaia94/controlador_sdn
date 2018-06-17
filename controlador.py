ecd4983b48ea9dd57e3c899e659737ad3741de7b
None <None>
2018-06-17 10:56:12 -0700

    mudanca na topologia e no controlador

--- controlador.py
+++ controlador.py
@@ -8,17 +8,13 @@ from ryu.lib.packet import ethernet
 from ryu.lib import hub
 
 class OFPHandler(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-    
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]    
 	
     def __init__(self, *args, **kwargs):
-        super(OFPHandler, self).__init__(*args, **kwargs)	
+        super(OFPHandler, self).__init__(*args, **kwargs)
 	# switches da rede
 	self.datapaths = {}
 
-        # inicializa a tabela de endereco MAC
-        self.mac_to_port = {}
-	
 	# registra velocidade das portas
 	self.port_speed = {}
 
@@ -65,7 +61,7 @@ class OFPHandler(app_manager.RyuApp):
         req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
         datapath.send_msg(req)
     
-    #Recebe respostas das requisocoes de flow stats
+    #Recebe respostas das requisicoes de flow stats
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
         body = ev.msg.body
@@ -85,7 +81,7 @@ class OFPHandler(app_manager.RyuApp):
                              stat.instructions[0].actions[0].port,
                              stat.packet_count, stat.byte_count)
 
-    #Recebe respostas das requisocoes de port stats
+    #Recebe respostas das requisicoes de port stats
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
     def _port_stats_reply_handler(self, ev):
         body = ev.msg.body
@@ -124,6 +120,24 @@ class OFPHandler(app_manager.RyuApp):
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                 match=match, instructions=inst)
         datapath.send_msg(mod)
+	
+    #Verifica para qual porta o pacote deve ser encaminhado
+    def set_out_port(self, dpid, dst):
+	if(dpid == '1'): 
+	    return 2
+	elif(dpid == '2'): 
+	    return 2
+	elif(dpid == '3'):
+	    return 3
+	elif(dpid == '4'):
+	    if(dst == '00:00:00:00:00:02'):
+		return 2
+	    elif(dst == '00:00:00:00:00:03'):
+		return 1
+	elif(dpid == '5'):
+	    return 3
+	elif(dpid == 6):
+	    return 3		    	
 
     #Evento de packet_in, switch envia essa mensagem para o controlador ao receber um pacote
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -133,38 +147,27 @@ class OFPHandler(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # id do switch atual
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
-
         # extrai informacoes do pacote
         pkt = packet.Packet(msg.data)
         eth_pkt = pkt.get_protocol(ethernet.ethernet)
         dst = eth_pkt.dst
         src = eth_pkt.src
 
+        # determina qual porta o pacote deve ser encaminhado
+	out_port = self.set_out_port(datapath.id, dst)
+	
         # porta pela qual switch recebeu o pacote
         in_port = msg.match['in_port']
-	
+
 	# loga os dados do pacote
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-
-        # aprende o mac_address para evitar FLOOD
-        self.mac_to_port[dpid][src] = in_port
-
-	# verifica se conhece porta para encaminhar pacote, caso contrario FLOOD
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
 
         # controi lista de acoes
         actions = [parser.OFPActionOutput(out_port)]
 
         # instala regra no switch
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            self.add_flow(datapath, 1, match, actions)
+        match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+        self.add_flow(datapath, 1, match, actions)
 
         # encaminha o pacote
         out = parser.OFPPacketOut(datapath=datapath,
